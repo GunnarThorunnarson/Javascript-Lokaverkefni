7be383946d57d5bfe0fa69ee3f0b2004af679c96
KyplerEspiritu <kypleres312@nemi.tskoli.is>
2018-04-24 19:09:05 +0000

    UI update and time of day added

--- scripts/apis_data.js
+++ scripts/apis_data.js
@@ -4,6 +4,8 @@
 let eventName = [];
 let locationName = [];
 let dateShow = [];
+let day = [];
+let hour = [];
 let imageSrc = [];
 
 $.getJSON('http://apis.is/concerts', function(data) {
@@ -13,6 +15,11 @@ $.getJSON('http://apis.is/concerts', function(data) {
     	dateShow.push(data['results'][i]['dateOfShow']);
     	imageSrc.push(data['results'][i]['imageSource']);
 	}
+
+	dateShow.forEach(function (time_date){
+		day.push(time_date.split('T')[0]);
+		hour.push(time_date.split('T')[1]);
+	})
 
 	for (let x = 0; x < data['results'].length; x++){
 
@@ -33,6 +40,9 @@ $.getJSON('http://apis.is/concerts', function(data) {
 		let strongName = document.createElement('strong');
 		let infoNameText = document.createTextNode('Nafn:');
 		strongName.appendChild(infoNameText);
+		if (eventName[x].length > 25){
+			eventName[x] = eventName[x].substring(0, 25) + '...';
+		}
 		let event = document.createTextNode(' ' + eventName[x]);
 		infoName.appendChild(strongName);
 		infoName.appendChild(event);
@@ -42,6 +52,9 @@ $.getJSON('http://apis.is/concerts', function(data) {
 		let strongLoca = document.createElement('strong');
 		let infoLocaText = document.createTextNode('Staðsetning:');
 		strongLoca.appendChild(infoLocaText);
+		if (locationName[x].length > 25){
+			locationName[x] = locationName[x].substring(0, 25) + '...';
+		}
 		let loaction = document.createTextNode(' ' + locationName[x]);
 		infoLoca.appendChild(strongLoca);
 		infoLoca.appendChild(loaction);
@@ -49,13 +62,21 @@ $.getJSON('http://apis.is/concerts', function(data) {
 
 		let infoDate = document.createElement('div');
 		let strongDate = document.createElement('strong');
-		let infoDateText = document.createTextNode('Dagsetning:');
+		let infoDateText = document.createTextNode('Dagur:');
 		strongDate.appendChild(infoDateText);
-		let date = document.createTextNode(' ' + dateShow[x]);
+		let date = document.createTextNode(' ' + day[x]);
 		infoDate.appendChild(strongDate);
 		infoDate.appendChild(date);
 		infoElem.appendChild(infoDate);
 
+		let infoTime = document.createElement('div');
+		let strongTime = document.createElement('strong');
+		let infoTimeText = document.createTextNode('Tími:');
+		strongTime.appendChild(infoTimeText);
+		let time = document.createTextNode(' ' + hour[x]);
+		infoTime.appendChild(strongTime);
+		infoTime.appendChild(time);
+		infoElem.appendChild(infoTime);
 
 		document.body.appendChild(oneConcert);
 
